b78ae9399ac003a6536189f60014d055fe622ac0
obi3965 <46780156+obi3965@users.noreply.github.com>
2020-12-14 12:22:44 +0100

    product model is created

---
+++ server/models/product.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+
+const { ObjectId } = mongoose.Schema
+
+const productSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        maxLength: 32,
+        trim:true
+    },
+    description: {
+        type: String,
+        required:true,
+        maxLength: 2000
+      },
+
+      price:{
+        type:Number,
+        maxLength: 32,
+        trim:true,
+        required:true
+    },
+    category: {
+        type: ObjectId,
+        ref:'Category',
+        required:true
+      },
+    quantity:{
+        type: Number
+    },
+    photo:{
+        data: Buffer,
+        contentType:String
+    },
+    shipping:{
+        required:false,
+        type:Boolean
+    }
+    
+}, { timestamps: true})
+
+
+module.exports = mongoose.model('Product', productSchema)
