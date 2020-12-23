0aeec5733346b06eaf037ee1db84005ec07b1245
obi3965 <46780156+obi3965@users.noreply.github.com>
2020-12-23 22:55:17 +0100

    user profile update and single user is done

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -12,8 +12,30 @@ exports.userById = (req, res, next, id) => {
   });
 };
 
+//read the user profile
 exports.read = (req, res) => {
   req.profile.hashed_password = undefined;
   req.profile.salt = undefined;
   return res.json(req.profile);
 };
+
+
+//update the user profile
+exports.updateUser = (req,res) => {
+  User.findByIdAndUpdate(
+    {_id: req.profile._id},
+    {$set: req.body},
+    {new: true},
+    (err, user) =>{
+      if(err){
+        return res.status(400).json({
+          error: 'you are not authorized to perform this action'
+        })
+      }
+      user.hashed_password = undefined;
+      user.salt = undefined
+      res.json(user)
+    }
+
+  )
+}
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,10 +3,12 @@ const express = require('express')
 const router = express.Router()
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth')
-const { userById, read } = require('../controllers/user')
+const { userById, read, updateUser } = require('../controllers/user')
 
 
-router.get('/user/:userId', requireSignin, isAuth, isAdmin, read )
+//router.get('/user/:userId', requireSignin, isAuth, isAdmin, read )
+router.get('/user/:userId', requireSignin, isAuth, read )
+router.put('/user/:userId', requireSignin, isAuth, updateUser )
 router.param('userId', userById)
 
 
