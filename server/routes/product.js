5d398f104b28be3f9e2073c0a5d41577cd6d59b7
obi3965 <46780156+obi3965@users.noreply.github.com>
2020-12-23 11:42:03 +0100

    product by search is done

--- server/routes/product.js
+++ server/routes/product.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = express.Router()
 
 
-const { create, singleProduct, read, remove, updateProduct, list, listRelated, listCategories } = require('../controllers/product')
+const { create, singleProduct, read, remove, updateProduct, list, listRelated, listCategories, listBySearch, photo } = require('../controllers/product')
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
@@ -13,6 +13,8 @@ router.get('/products', list)
 router.get('/product/:productId', read )
 router.get('/products/related/:productId', listRelated )
 router.get('/products/categories', listCategories)
+router.post("/products/bySearch", listBySearch);
+router.get('/products/photo/:productId', photo)
 router.post('/product/create/:userId', requireSignin, isAuth,isAdmin, create)
 router.delete('/product/:productId/:userId', requireSignin, isAuth,isAdmin, remove )
 router.put('/product/:productId/:userId', updateProduct)
