fb65508a548086a75dbb10158677a3dcab6dbba5
obi3965 <46780156+obi3965@users.noreply.github.com>
2021-01-07 15:35:27 +0100

    braintree payment proccess for getting the token is done

--- server/app.js
+++ server/app.js
@@ -14,6 +14,7 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const categoryRoutes = require("./routes/category");
 const productRoutes = require("./routes/product");
+const braintreeRoutes = require('./routes/braintree');
 
 // middleware -
 app.use(logger("dev"));
@@ -39,6 +40,8 @@ app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
+app.use('/api', braintreeRoutes);
+
 
 //start the server
 const port = 4000;
